46fa1b018e32a4325e7128c03d130665e9338ccc
fabioscap <fabiovt_99@hotmail.it>
2022-07-06 11:42:47 +0200

    added naked man and sky

--- src/scene.js
+++ src/scene.js
@@ -17,17 +17,48 @@ var createScene = function () {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
+    var skyMaterial = new BABYLON.SkyMaterial("skyMaterial", scene);
+    skyMaterial.backFaceCulling = false;
     
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+    skybox.material = skyMaterial;
 
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-    
+    BABYLON.SceneLoader.Append("","./res/models/man.obj", scene);
 
 
     // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+    // var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
+    var setSkyConfig = function (property, from, to) {
+		var keys = [
+            { frame: 0, value: from },
+			{ frame: 100, value: to }
+        ];
+		
+		var animation = new BABYLON.Animation("animation", property, 100, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+		animation.setKeys(keys);
+		
+		scene.stopAnimation(skybox);
+		scene.beginDirectAnimation(skybox, [animation], 0, 100, false, 1);
+	};
+
+    setSkyConfig("material.inclination", skybox.material.inclination, 0);
+
+    window.addEventListener("keydown", function (evt) {
+		switch (evt.keyCode) {
+			case 49: setSkyConfig("material.inclination", skybox.material.inclination, 0); break; // 1
+			case 50: setSkyConfig("material.inclination", skybox.material.inclination, -0.5); break; // 2
+
+			case 51: setSkyConfig("material.luminance", skybox.material.luminance, 0.1); break; // 3
+			case 52: setSkyConfig("material.luminance", skybox.material.luminance, 1.0); break; // 4
+			
+			case 53: setSkyConfig("material.turbidity", skybox.material.turbidity, 40); break; // 5
+			case 54: setSkyConfig("material.turbidity", skybox.material.turbidity, 5); break; // 6
+			
+            case 55: setSkyConfig("material.cameraOffset.y", skybox.material.cameraOffset.y, 50); break; // 7
+            case 56: setSkyConfig("material.cameraOffset.y", skybox.material.cameraOffset.y, 0); break;  // 8
+			default: break;
+		}
+    });
     return scene;
 };
