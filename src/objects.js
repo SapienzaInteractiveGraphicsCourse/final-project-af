e6c2a94663e9247535be2c23da7c89b6613a83b5
Giallored <pacciarelli.1816493@studenti.uniroma1.it>
2022-07-09 17:42:34 +0200

    fixed world

--- src/objects.js
+++ src/objects.js
@@ -9,7 +9,7 @@ function createHouse(scene) {
         object =  scene.getMeshById("House");
         object.position.z = 10;
         object.position.y = -0.3;
-        
+        console.log(object.rotation);
     });
 
     object =  scene.getMeshById("House");
@@ -34,8 +34,123 @@ function createTree(scene,position,rotation) {
         object.rotation.z = rotation[2];
         object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
     });
+    
+    return object;
+}
 
-    //object =  scene.getMeshById("dead_tree");
+function createTractor(scene,position,rotation) {
+    var object = null;
+    
+    BABYLON.SceneLoader.Append("./res/models/tractor-babylon/","tractor.babylon", scene,function(scene) {
+        object =  scene.getMeshById("tractor_root");
+        object.position.x = position[0];
+        object.position.y = position[1];
+        object.position.z = position[2];
+        object.scaling.x = 0.4;
+        object.scaling.y = 0.4;
+        object.scaling.z = 0.4;
+        object.rotation.x = rotation[0];
+        object.rotation.y = rotation[1];
+        object.rotation.z = rotation[2];
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
+
+    return object;
+}
+
+
+function createStone(scene,position,rotation) {
+    var object = null;
+
+    BABYLON.SceneLoader.Append("./res/models/stones-babylon/","stones.babylon", scene,function(scene) {
+        let object =  scene.getMeshById("Fbx_Root");
+        object.position.x = position[0];
+        object.position.y = position[1];
+        object.position.z = position[2];
+        object.scaling.x = 0.01;
+        object.scaling.y = 0.01;
+        object.scaling.z = 0.01;
+        object.rotation.x = rotation[0];
+        object.rotation.y = rotation[1];
+        object.rotation.z = rotation[2];
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
+    
+    return object;
+}
+
+
+function createPlanets(scene) {
+    
+    BABYLON.SceneLoader.Append("./res/models/blue-planet-babylon/","blue-planet.babylon", scene,function(scene) {
+        let object =  scene.getMeshById("Fleka_planet_1");
+        object.position = new BABYLON.Vector3(-20,30,-100);
+        object.scaling.x = 5;
+        object.scaling.y = 5;
+        object.scaling.z = 5;
+        object.rotation.x = 0;
+        object.rotation.y = Math.PI;
+        object.rotation.z = Math.PI;
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
+
+    
+
+    var redMat = new BABYLON.StandardMaterial("redMat", scene);
+	redMat.diffuseTexture = new BABYLON.Texture("./res/textures/terradisiena.jpg", scene);
+	
+	var greenMat = new BABYLON.StandardMaterial("greenMat", scene);
+	greenMat.diffuseTexture = new BABYLON.Texture("./res/textures/ground.jpg", scene);
+
+    var sandMat = new BABYLON.StandardMaterial("sandMat", scene);
+	sandMat.diffuseTexture = new BABYLON.Texture("./res/textures/sand.jpg", scene);
+	
+    var terraMat = new BABYLON.StandardMaterial("terraMat", scene);
+	terraMat.diffuseTexture = new BABYLON.Texture("./res/textures/terracotta.jpeg", scene);
+
+	var sphere1 = BABYLON.MeshBuilder.CreateSphere("sphere1", {diameter:50}, scene);
+	sphere1.material = redMat;
+	sphere1.position  = new BABYLON.Vector3(100,10,100);
+    sphere1.rotation.y = -Math.PI/2;
+
+    
+	var sphere2 = BABYLON.MeshBuilder.CreateSphere("sphere2", {diameter:60}, scene);
+	sphere2.material = greenMat;
+    sphere2.position = new BABYLON.Vector3(-150,-80,100);
+    sphere2.rotation.y = Math.PI;
+
+    var sphere3 = BABYLON.MeshBuilder.CreateSphere("sphere3", {diameter:60}, scene);
+	sphere3.material = terraMat;
+    sphere3.position = new BABYLON.Vector3(-100,-30,-100);
+    sphere3.rotation.y = Math.PI/2
+
+    const ring = BABYLON.MeshBuilder.CreateTorus("torus", {thickness: 10, diameter: 100, tessellation:32});
+    ring.material = sandMat;
+    ring.position = new BABYLON.Vector3(-100,-30,-100);
+    ring.rotation.x = -Math.PI/6;
+
+
+}
+
+
+
+function createApollo(scene,position,rotation) {
+    var object = null;
+    
+    BABYLON.SceneLoader.Append("./res/models/apollo-babylon/","apollo.babylon", scene,function(scene) {
+        let object =  scene.getMeshById("Fbx_Root");
+        rename_with_prefix(object, "apollo")
+        object.position.x = position[0];
+        object.position.y = position[1];
+        object.position.z = position[2];
+        object.scaling.x = 1.0;
+        object.scaling.y = 1.0;
+        object.scaling.z = 1.0;
+        object.rotation.x = rotation[0];
+        object.rotation.y = rotation[1];
+        object.rotation.z = rotation[2];
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
     
     return object;
 }
@@ -67,104 +182,6 @@ function getRotation(position,worldC){
 }
 
 
-function createTractor(scene,position,rotation) {
-    var object = null;
-    
-    //BABYLON.SceneLoader.Append("./res/models/tree-05-babylon/","tree-05.babylon", scene,function(scene) {
-        BABYLON.SceneLoader.Append("./res/models/tractor-babylon/","tractor.babylon", scene,function(scene) {
-        object =  scene.getMeshById("tractor_root");
-        object.position.x = position[0];
-        object.position.y = position[1];
-        object.position.z = position[2];
-        object.scaling.x = 0.4;
-        object.scaling.y = 0.4;
-        object.scaling.z = 0.4;
-        object.rotation.x = rotation[0];
-        object.rotation.y = rotation[1];
-        object.rotation.z = rotation[2];
-        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
-        
-    });
-
-    //object =  scene.getMeshById("dead_tree");
-    
-    return object;
-}
-
-
-function createStone(scene,position,rotation) {
-    var object = null;
-    
-    //BABYLON.SceneLoader.Append("./res/models/tree-05-babylon/","tree-05.babylon", scene,function(scene) {
-        BABYLON.SceneLoader.Append("./res/models/stones-babylon/","stones.babylon", scene,function(scene) {
-        let object =  scene.getMeshById("Fbx_Root");
-        object.position.x = position[0];
-        object.position.y = position[1];
-        object.position.z = position[2];
-        object.scaling.x = 0.01;
-        object.scaling.y = 0.01;
-        object.scaling.z = 0.01;
-        object.rotation.x = rotation[0];
-        object.rotation.y = rotation[1];
-        object.rotation.z = rotation[2];
-        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
-        
-    });
-
-    //object =  scene.getMeshById("dead_tree");
-    
-    return object;
-}
-
-
-function createStone2(scene,position,rotation) {
-    var object = null;
-    
-    //BABYLON.SceneLoader.Append("./res/models/tree-05-babylon/","tree-05.babylon", scene,function(scene) {
-        BABYLON.SceneLoader.Append("./res/models/stone2-babylon/","stone2.babylon", scene,function(scene) {
-        object =  scene.getMeshById("Fbx_Root");
-        object.position.x = position[0];
-        object.position.y = position[1];
-        object.position.z = position[2];
-        object.scaling.x = 1.0;
-        object.scaling.y = 1.0;
-        object.scaling.z = 1.0;
-        object.rotation.x = rotation[0];
-        object.rotation.y = rotation[1];
-        object.rotation.z = rotation[2];
-        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
-        
-    });
-
-    //object =  scene.getMeshById("dead_tree");
-    
-    return object;
-}
-
-
-function createApollo(scene,position,rotation) {
-    var object = null;
-    
-    //BABYLON.SceneLoader.Append("./res/models/tree-05-babylon/","tree-05.babylon", scene,function(scene) {
-    BABYLON.SceneLoader.Append("./res/models/apollo-babylon/","apollo.babylon", scene,function(scene) {
-        let object =  scene.getMeshById("Fbx_Root");
-        object.position.x = position[0];
-        object.position.y = position[1];
-        object.position.z = position[2];
-        object.scaling.x = 1.0;
-        object.scaling.y = 1.0;
-        object.scaling.z = 1.0;
-        object.rotation.x = rotation[0];
-        object.rotation.y = rotation[1];
-        object.rotation.z = rotation[2];
-        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
-        
-    });
-
-    //object =  scene.getMeshById("dead_tree");
-    
-    return object;
-}
 
 function createRandomTrees(scene,position,rotation){
     var object = null;
@@ -180,7 +197,147 @@ function createRandomTrees(scene,position,rotation){
     });
 }
 
-// Shift+Ctrl+Alt+I to open the inspector
-window.addEventListener("keydown", (ev) => {
-    if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) 
-    if (this.scene.debugLayer.isVisible()) this.scene.debugLayer.hide(); else this.scene.debugLayer.show();});
+
+
+function createChair(scene,position,rotation){
+    var object = null;
+    BABYLON.SceneLoader.Append("./res/models/chair-babylon/","chair.babylon", scene,function(scene) {
+        object =  scene.getMeshById("3DSMesh_0_0");
+        object.position.x = position[0];
+        object.position.y = position[1];
+        object.position.z = position[2];
+        object.scaling.x = 0.22;
+        object.scaling.y = 0.22;
+        object.scaling.z = 0.22;
+        object.rotation.x = rotation[0];
+        object.rotation.y = rotation[1];
+        object.rotation.z = rotation[2];
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
+
+    return object;
+}
+
+
+function createCristals(scene,R, worldC){
+    var object = null;
+    BABYLON.SceneLoader.Append("./res/models/three-crystals-babylon/","three-crystals.babylon", scene,function(scene) {
+        object =  scene.getMeshById("kristalli_03_fixed__1_");
+        object.position.x = R + worldC[0] + 3.5;
+        object.position.y = worldC[1];
+        object.position.z = worldC[2];
+        object.scaling.x = 0.1;
+        object.scaling.y = 0.1;
+        object.scaling.z = 0.1;
+        object.rotation.x = 0;
+        object.rotation.y = 0;
+        object.rotation.z = -Math.PI/2;
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
+
+    BABYLON.SceneLoader.Append("./res/models/blue-crystal-babylon/","blue-crystal.babylon", scene,function(scene) {
+        object =  scene.getMeshById("root");
+        object.position.x =  worldC[0] - R-1.0;
+        object.position.y = worldC[1]-6.0;
+        object.position.z = worldC[2] + 5.0;
+        object.scaling.x = 30;
+        object.scaling.y = 30;
+        object.scaling.z = 30;
+        object.rotation.x = Math.PI/2;
+        object.rotation.y = Math.PI/2;
+        object.rotation.z = 0;
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
+    
+}
+
+function createGrave(scene,worldC,R){
+
+    BABYLON.SceneLoader.Append("./res/models/grave-babylon/","grave.babylon", scene,function(scene) {
+        object =  scene.getMeshById("Stone_Wall");
+        object.position.x =  worldC[0];
+        object.position.y = worldC[1] - R ;
+        object.position.z = worldC[2] - 5.5;
+        object.scaling.x = 5;
+        object.scaling.y = 5;
+        object.scaling.z = 5;
+        object.rotation.x = Math.PI;
+        object.rotation.y = 0;
+        object.rotation.z = 0;
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
+
+    BABYLON.SceneLoader.Append("./res/models/crystals-babylon/","crystals.babylon", scene,function(scene) {
+        object =  scene.getMeshById("crystal_root");
+        object.position.x =  worldC[0] -5.2;
+        object.position.y = worldC[1] -R ;
+        object.position.z = worldC[2] -4.5;
+        object.scaling.x = 13;
+        object.scaling.y = 13;
+        object.scaling.z = 13;
+        object.rotation.x = Math.PI;
+        object.rotation.y = 0;
+        object.rotation.z = 0;
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
+
+
+    BABYLON.SceneLoader.Append("./res/models/ruby-babylon/","ruby.babylon", scene,function(scene) {
+        object =  scene.getMeshById("crystal");
+        object.position.x =  worldC[0] + 1.0 ;
+        object.position.y = worldC[1] - R;
+        object.position.z = worldC[2] + 2.0;
+        object.scaling.x = 3;
+        object.scaling.y = 3;
+        object.scaling.z = 3;
+        object.rotation.x = Math.PI;
+        object.rotation.y = 0;
+        object.rotation.z = 0;
+        object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
+    });
+
+
+    var particleSystem;
+
+    var fogTexture = new BABYLON.Texture("./res/textures/smoke.png", scene);
+
+    var createNewSystem = function() {
+    if (particleSystem) {
+        particleSystem.dispose();
+    }
+
+    var fountain = BABYLON.Mesh.CreateBox("foutain", .01, scene);
+    fountain.position = new BABYLON.Vector3(0.0, -2*R + 1.0, 10.0);
+    fountain.visibility = 0;
+
+    particleSystem = new BABYLON.ParticleSystem("particles", 2500 , scene);
+    particleSystem.manualEmitCount = particleSystem.getCapacity();
+    particleSystem.minEmitBox = new BABYLON.Vector3(-5, 1, -5); // Starting all from
+    particleSystem.maxEmitBox = new BABYLON.Vector3(5, 1, 5); // To...
+
+    particleSystem.particleTexture = fogTexture.clone();
+    particleSystem.emitter = fountain;
+    
+    particleSystem.color1 = new BABYLON.Color4(0.8, 0.8, 0.8, 0.1);
+    particleSystem.color2 = new BABYLON.Color4(.95, .95, .95, 0.15);
+    particleSystem.colorDead = new BABYLON.Color4(0.9, 0.9, 0.9, 0.1);
+    particleSystem.minSize = 7.0;
+    particleSystem.maxSize = 10.0;
+    particleSystem.minLifeTime = Number.MAX_SAFE_INTEGER;
+    particleSystem.emitRate = 50000;
+    particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_STANDARD;
+    particleSystem.gravity = new BABYLON.Vector3(0, 0, 0);
+    particleSystem.direction1 = new BABYLON.Vector3(0, 0, 0);
+    particleSystem.direction2 = new BABYLON.Vector3(0, 0, 0);
+    particleSystem.minAngularSpeed = -2;
+    particleSystem.maxAngularSpeed = 2;
+    particleSystem.minEmitPower = .5;
+    particleSystem.maxEmitPower = 1;
+    particleSystem.updateSpeed = 0.005;
+
+    particleSystem.start();
+    }
+
+    createNewSystem();
+
+}
--- src/scenePlanet.js
+++ src/scenePlanet.js
@@ -46,33 +46,65 @@ var createScene = function () {
     worldMat.ambientTexture = worldText;
     worldMat.specularColor = new BABYLON.Color3(0,0,0);
 
-    var columns = 30;  // 6 columns
-    var rows = 30;  // 4 rows
+    var underworldMat = new BABYLON.StandardMaterial("mat2", scene);
+    var underworldText = new BABYLON.Texture("./res/textures/vulcan.jpg",scene);
+    underworldMat.ambientTexture = underworldText;
+    underworldMat.specularColor = new BABYLON.Color3(0,0,0);
 
-    var faceUV = new Array(columns*rows);
-    
+    var barrierMat = new BABYLON.StandardMaterial("barrierMat", scene);
+	barrierMat.specularColor = new BABYLON.Color3(221,160,221);
+    //barrierMat.diffuseTexture = new BABYLON.Texture(".res/textures/fuxya.png", scene);
+	barrierMat.alpha = 0.3;
+
     
     const R = 30.0;
     const world_position = new BABYLON.Vector3(0.0, -R, 10.0);
-    const world_rotation =  new BABYLON.Vector3(Math.PI/2 , Math.PI/2, Math.PI/2);
+    const world_rotation =  new BABYLON.Vector3(Math.PI/2,Math.PI, Math.PI/2);
     const world = BABYLON.MeshBuilder.CreateSphere("world", {diameter:R*2}, scene);
     world.position = world_position;
     world.rotation = world_rotation;
-
     world.material = worldMat;
 
+    const underworld_rotation =  new BABYLON.Vector3( 0 , 0,Math.PI);
+    const underworld = BABYLON.MeshBuilder.CreateSphere("underworld", {diameter:R*2+0.1, slice: 0.5});
+    underworld.position = world_position;
+    underworld.rotation = underworld_rotation;
+    underworld.material = underworldMat;
+
+    const barrier = BABYLON.MeshBuilder.CreateCylinder("barrier", {height :0.02, diameter:R*2 + 10, tessellation:48}, scene);
+    barrier.position = world_position;
+    var purple = new BABYLON.Color4(221/255,160/255,221/255,1);
+    barrier.faceColors = [purple,purple,purple]
+    barrier.material =  barrierMat;
+
+    createPlanets(scene);
+
+
     var house = createHouse(scene);
-    
-    //var tree = createTree(scene,[-10,-4,20],[2*Math.PI/5,Math.PI/5,0]);
-    //var tree2 = createTree(scene,[10,0,0],[0,Math.PI/3,0]);
-    //var tree3 = createTree(scene,[0,10,0],[0,Math.PI/3,0]);
+    var chair = createChair(scene,[-5,-0.5,10],[0,Math.PI/2,-Math.PI/25]);
+    var crystal = createCristals(scene,R, [0.0, -R, 10.0]);
+
     var tractor = createTractor(scene,[6,-0.85,7],[-Math.PI/25,0,-Math.PI/20]);
     var stone = createStone(scene,[-10,-3.6,0],[-Math.PI/10,Math.PI/20,0]);
+    
+    var position_random_tree = getRandomLoc([0.0, -R, 10.0],R);
+    var rotation_random_tree = getRotation(position_random_tree,world_position);
+    var tree = createRandomTrees(scene,position_random_tree,rotation_random_tree);
 
-    var position_random = getRandomLoc([0.0, -R, 10.0],R);
-    var rotation_random = getRotation(position_random,world_position);
-    var tree = createRandomTrees(scene,position_random,rotation_random);
+    var position_random_stone = getRandomLoc([0.0, -R, 10.0],R);
+    var rotation_random_stone = getRotation(position_random_stone,world_position);
 
-    //var apollo = createApollo(scene,[0,-R,10.25 - R],[-Math.PI/2,0,0]);
+    var tomb = createGrave(scene,[0.0, -R, 10.0],R)
+
+
+    window.addEventListener("keydown", (ev) => {
+    if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) 
+    if (this.scene.debugLayer.isVisible()) this.scene.debugLayer.hide(); else this.scene.debugLayer.show();});
+    // Shift+Ctrl+Alt+I to open the inspector
+
+    
+
+    
+
     return scene;
 };
