de84bf6cf9207dd5c2118735926da9088a965eb1
fabioscap <fabiovt_99@hotmail.it>
2022-07-09 13:28:24 +0200

    random location

--- src/objects.js
+++ src/objects.js
@@ -9,7 +9,7 @@ function createHouse(scene) {
         object =  scene.getMeshById("House");
         object.position.z = 10;
         object.position.y = -0.3;
-        console.log(object.rotation);
+        
     });
 
     object =  scene.getMeshById("House");
@@ -42,20 +42,27 @@ function createTree(scene,position,rotation) {
 
 
 function getRandomLoc(worldC,R){
-    var x = (Math.cos(Math.random()*Math.PI))* R + worldC[0] ;
-    var z = (Math.cos(Math.random()*Math.PI)) * R + worldC[2] ;
-    var y = worldC[1] + Math.sqrt( R*R - Math.pow((x - worldC[0]),2) - Math.pow((z - worldC[2]),2));
-    return BABYLON.Vector3(x,y,z);
+    var phi = Math.random()*Math.PI;
+    var theta = Math.random()*2*Math.PI;
+    var x = R*Math.sin(phi)*Math.cos(theta) + worldC[0];
+    var y = R*Math.sin(phi)*Math.sin(theta) + worldC[1];
+    var z = R*Math.cos(phi)                 + worldC[2];
+
+    return new BABYLON.Vector3(x,y,z)
 }
 
 function getRotation(position,worldC){
-    //var z_axis = normalize(position.subtract(worldC));
-    var random_vector =  BABYLON.Vector3(Math.random(),Math.random(),Math.random());
-    //var x_axis = z_axis.cross(r);
-    //var y_axis = x_axis.cross(z_axis);
-    console.log(random_vector);
-    var m = BABYLON.Matrix.LookAtRH(worldC, position, random_vector);
 
+    var z = worldC.subtract(position).normalize();
+    var random_vector =  new BABYLON.Vector3(Math.random(),Math.random(),Math.random()).normalize();
+
+    var x = (z.cross(random_vector)).normalize();
+    var y = x.cross(z).normalize(); 
+
+    var m = new BABYLON.Matrix();
+    m.setRow(0,new BABYLON.Vector4(x._x,x._y,x._z,0))
+    m.setRow(1,new BABYLON.Vector4(y._x,y._y,y._z,0))
+    m.setRow(2,new BABYLON.Vector4(z._x,z._y,z._z,0))
     return m;
 }
 
@@ -76,7 +83,7 @@ function createTractor(scene,position,rotation) {
         object.rotation.y = rotation[1];
         object.rotation.z = rotation[2];
         object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
-        console.log(object);
+        
     });
 
     //object =  scene.getMeshById("dead_tree");
@@ -101,7 +108,7 @@ function createStone(scene,position,rotation) {
         object.rotation.y = rotation[1];
         object.rotation.z = rotation[2];
         object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
-        console.log(object);
+        
     });
 
     //object =  scene.getMeshById("dead_tree");
@@ -126,7 +133,7 @@ function createStone2(scene,position,rotation) {
         object.rotation.y = rotation[1];
         object.rotation.z = rotation[2];
         object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
-        console.log(object);
+        
     });
 
     //object =  scene.getMeshById("dead_tree");
@@ -151,7 +158,7 @@ function createApollo(scene,position,rotation) {
         object.rotation.y = rotation[1];
         object.rotation.z = rotation[2];
         object.rotationQuaternion = BABYLON.Quaternion.FromEulerVector(object.rotation);
-        console.log(object);
+        
     });
 
     //object =  scene.getMeshById("dead_tree");
@@ -164,14 +171,16 @@ function createRandomTrees(scene,position,rotation){
      
     BABYLON.SceneLoader.Append("./res/models/","looe.babylon", scene,function(scene) {
         object =  scene.getMeshById("Circle_Circle");
-        
-        m.decompose(null,object.rotationQuaternion,null,null);
 
-        object.position.x = position[0];
-        object.position.y = position[1];
-        object.position.z = position[2];
+        object.position = position;
         object.scaling.x = 0.7;
         object.scaling.y = 0.7;
         object.scaling.z = 0.7;
+        rotation.decompose(null,object.rotationQuaternion,null,null);
     });
 }
+
+// Shift+Ctrl+Alt+I to open the inspector
+window.addEventListener("keydown", (ev) => {
+    if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) 
+    if (this.scene.debugLayer.isVisible()) this.scene.debugLayer.hide(); else this.scene.debugLayer.show();});
