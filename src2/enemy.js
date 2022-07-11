71eb3398144f0f3e2d1c8ab29187a9dfc67ba2ff
fabioscap <fabiovt_99@hotmail.it>
2022-07-11 20:48:55 +0200

    added enemy animation

--- src2/enemy.js
+++ src2/enemy.js
@@ -11,6 +11,19 @@ function Enemy(scene,environment,player) {
                 (result) =>{
                     this.enemyAssets = result.meshes[0];
                     this.enemyAssets.isVisible = false; // i want to instance it
+
+                    this.floatingAnimation = new BABYLON.Animation("e_float","position.y",100,
+                    BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+                    BABYLON.Animation.ANIMATIONLOOPMODE_CYCLING);
+                    this.floatingAnimation.setKeys([
+                        { frame: 0,   value: 1.0},
+                        { frame: 50,  value: 1.5},
+                        { frame: 100, value: 1.0}
+                    ]);
+                    var easing = new BABYLON.SineEase();
+
+                    this.floatingAnimation.setEasingFunction(easing);
+                    this.enemyAssets.animations.push(this.floatingAnimation)
                 });
     }
     this.instanceEnemy = function(where) {
@@ -19,28 +32,31 @@ function Enemy(scene,environment,player) {
 
         var newInstance = this.enemyAssets.createInstance(index);
         newInstance.rotationQuaternion = null;
-            //newInstance.scaling = new BABYLON.Vector3(0.1,0.1,0.1);
+        this.scene.beginAnimation(newInstance, 0, 100, true,0.5);
+        //newInstance.scaling = new BABYLON.Vector3(0.1,0.1,0.1);
 
-            // "where has to be on the sphere
-            var R = this.environment.planet.radius;
-            console.log(R);
-            console.log(where.length())
-            if (Math.abs(where.length()-R) > 0.01) {
-                
-                where = where.normalize().scale(R);
-            }
+        // "where" has to be on the sphere
+        var R = this.environment.planet.radius;
+        console.log(R);
+        console.log(where.length())
+        if (Math.abs(where.length()-R) > 0.01) {
+            
+            where = where.normalize().scale(R);
+        }
 
-            base.position = where;
-            base.parent = this.environment.planet;
+        base.position = where;
+        base.parent = this.environment.planet;
 
-            newInstance.position._y += 1.0;
-            newInstance.rotation._y += Math.PI;
-            newInstance.parent = base;
+        newInstance.position._y += 1.0;
+        newInstance.rotation._y += Math.PI;
+        newInstance.parent = base;
 
-            base.rotation = new BABYLON.Vector3(0,Math.PI/2,-Math.PI/2);
+        base.rotation = new BABYLON.Vector3(0,Math.PI/2,-Math.PI/2);
 
-            this.enemies.push(newInstance);
+        this.enemies.push(newInstance);
     }
+
+        
 
     this.scene.onBeforeRenderObservable.add(() => {
         // here add enemy behavior
